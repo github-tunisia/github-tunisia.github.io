f70cd5890785654786a86135cf8c02fa26ae68a2
imed jaberi <imed_jebari@hotmail.fr>
2019-06-22 16:10:29 +0200

    add some effect using js for more interactive ♻️ ..

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+
+const TitleEffect = () => {
+   document.querySelector("h1").innerHTML = " Welcom To Our Org. 🎉 ";
+}
+
+const TitleOrigin = () => {
+   document.querySelector("h1").innerHTML = " Github Tunisia ";
+}
